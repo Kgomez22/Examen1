e75e5d05fd6d8624e03a6157b222c88197a6d933
user <user@DESKTOP-R3LAHUF>
2025-03-12 13:02:22 -0500

    Agregar archivos de proyecto.

---
+++ Controllers/VehiculoController.cs
@@ -0,0 +1,61 @@
+﻿using Examen1.clases;
+using Examen1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+
+namespace Examen1.Controllers
+{
+    [RoutePrefix("api/vehiculo")]
+    public class VehiculoController : ApiController
+    {
+        [HttpGet]
+        public async Task<List<VEHICULO>> GetAll()
+        {
+            clsVehiculo vehiculo = new clsVehiculo();
+            var result = new List<VEHICULO>();
+            return await vehiculo.ConsultarTodos();
+        }
+
+        [HttpGet]
+        [Route("consultar/{id}")]
+        public VEHICULO Get(int id) 
+        {
+            clsVehiculo vehiculo = new clsVehiculo();
+            return vehiculo.Consultar(id);
+        }
+
+        [HttpPost]
+        [Route("crear")]
+        public string Post([FromBody]VEHICULO vh) 
+        {
+            clsVehiculo vehiculo = new clsVehiculo();
+            vehiculo.vehiculo = vh;
+            return vehiculo.Insertar();
+        }
+
+        [HttpDelete]
+        [Route("eliminar/{id}")]
+        public string Delete(int id)
+        {
+            clsVehiculo vehiculo = new clsVehiculo();
+            vehiculo.vehiculo = new VEHICULO();
+            vehiculo.vehiculo.NUMERO_MOTOR = id;
+            return vehiculo.Eliminar();
+        }
+
+        [HttpPut]
+        [Route("actualizar")]
+        public string Update([FromBody] VEHICULO vh)
+        {
+            clsVehiculo vehiculo = new clsVehiculo();
+            vehiculo.vehiculo = vh;
+            return vehiculo.Actualizar();
+        }
+    }
+}
---
+++ clases/clsVehiculo.cs
@@ -0,0 +1,87 @@
+﻿using Examen1.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Xml.Linq;
+
+namespace Examen1.clases
+{
+    public class clsVehiculo
+    {
+        public ITM_VentasEntities Ventas = new ITM_VentasEntities();
+
+        public VEHICULO vehiculo { get; set; }
+
+        public string insertar()
+        {
+            Ventas.VEHICULOes.Add(vehiculo);
+            Ventas.SaveChanges();
+            return "vehiculo agregado correctamente";
+        }
+
+
+        public  string Insertar()
+        {
+            try
+            {
+                Ventas.VEHICULOes.Add(vehiculo);
+                Ventas.SaveChanges();
+                return "vehiculo agregado correctamente";
+            }
+            catch (Exception ex) 
+            {
+                return "Error al insertar el Vehiculo ";
+            }
+        }
+
+
+        public string Actualizar()
+        {
+            VEHICULO vEHICULO = new VEHICULO();
+            if (vEHICULO == null) 
+            {
+                return "El Vehiculo no es valido";
+            }
+            Ventas.VEHICULOes.AddOrUpdate(vehiculo);// actualiza un empleado en la tabla empleado
+            Ventas.SaveChanges(); //guarda los cambios en la base de datos
+            return " Se actualizao el Vehiculo correctamente"; //mensaje de confirmacion
+        }
+
+        public VEHICULO Consultar(int NUMERO_MOTOR)
+        {
+            VEHICULO Veh = Ventas.VEHICULOes.FirstOrDefault(e => e.NUMERO_MOTOR == NUMERO_MOTOR);
+            return Veh; 
+        }
+
+        public async Task<List<VEHICULO>> ConsultarTodos() 
+        {
+            var list = await Ventas.VEHICULOes.ToListAsync();
+            return list;
+
+        }
+
+        public string Eliminar()
+        {
+            try
+            {
+                //se debe consultar si exite el empleado
+                var ve = Consultar(vehiculo.NUMERO_MOTOR); //consultamos
+                if (ve == null)
+                {
+                    return "El vehiculo no existe";
+                }
+                Ventas.VEHICULOes.Remove(ve);
+                Ventas.SaveChanges();
+                return "Se elimino correctamente";
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+    }
+}
